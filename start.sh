38bfa79684137e6b9ef2e85ec0daba1c4c880af9
Y0ung_Sandwich <faist@outlook.com>
2019-08-25 16:50:41 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-# Minecraft
-# Author: YoungSancwich
-# http://mechandmagic.com
-
+# Mech&Magic
 [ $USER = "root" ] && echo 'WARNING Do Not Run This Script As Root!' && exit 1
 
 #Launch from server file directory
-screen -d -m -S mm -t world -d -m /path/to/directory/forge_start.sh
+screen -d -m -S mm -t world -d -m /mnt/raid0/mechmagic/forge_start.sh
 
 #Purge Crash Reports older than 4 days
-find /path/to/directory/crash-reports -mmin +5760 -exec rm {} \;
+find /mnt/raid0/mechmagic/crash-reports -mmin +5760 -exec rm {} \;
 
 #Purge Logs older than 4 days
-find /path/to/directory/logs -mmin +5760 -exec rm {} \;
+find /mnt/raid0/mechmagic/logs -mmin +5760 -exec rm {} \;
 
 
