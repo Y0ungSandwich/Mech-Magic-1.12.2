ddd9024767963b6de90001564fac2b10f0c3a43a
Y0ung_Sandwich <faist@outlook.com>
2019-08-25 16:51:10 -0400

    Update forge_start.sh

--- forge_start.sh
+++ forge_start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-###mm###
+###Mech Magic Lite###
+###This script is ran after start.sh###
 ###Create Java Virtual Machine###
-java -Xmx10G -XX:+UseG1GC -XX:+AlwaysPreTouch -XX:InitiatingHeapOccupancyPercent=20  -XX:+UseLargePagesInMetaspace  -Dfml.queryResult=confirm -jar forge-1.12.2-14.23.1.2607-universal.jar no gui
+java -Xmx16G -XX:+UseG1GC -XX:+AlwaysPreTouch -XX:InitiatingHeapOccupancyPercent=20  -XX:+UseLargePagesInMetaspace -Dfml.readTimeout=300 -Dfml.queryResult=confirm -jar forge-1.12.2-14.23.5.2836-universal.jar no gui
